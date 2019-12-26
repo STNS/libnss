ac3fdf9bfc0c7104d069089695a5dfb9c8e1a0b9
sugy <sugiyama@pepabo.com>
2019-12-26 13:12:46 +0900

    When code is not 0, Overwrite result with CURLE_HTTP_RETURNED_ERROR.
    - If the result of inner_http_request is CURLE_HTTP_RETURNED_ERROR, it will not be retried.

--- stns.c
+++ stns.c
@@ -344,7 +344,8 @@ static CURLcode inner_http_request(stns_conf_t *c, char *path, stns_response_t *
     res->data        = NULL;
     res->size        = 0;
     res->status_code = code;
-    result           = CURLE_HTTP_RETURNED_ERROR;
+    if (code != 0)
+      result = CURLE_HTTP_RETURNED_ERROR;
   }
 
 #ifdef DEBUG
